7294c42ac6b2c2232611e025ad64f15eacf63c7c
Stefan Haustein <stefan.haustein@gmail.com>
2016-06-12 20:15:38 +0200

    Simplify PixmapIO to use the internal canvas instead of trying to create a new one from the (currently broken) backing data

--- html/src/com/googlecode/gdxquake2/emul/com/badlogic/gdx/graphics/PixmapIO.java
+++ html/src/com/googlecode/gdxquake2/emul/com/badlogic/gdx/graphics/PixmapIO.java
@@ -6,14 +6,12 @@ import com.google.gwt.typedarrays.shared.ArrayBufferView;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.HasArrayBufferView;
 
 public class PixmapIO {
+
     public static void writePNG(FileHandle fileHandle, Pixmap pixmap) {
         try {
-            ArrayBufferView typedArray = ((HasArrayBufferView) pixmap.getPixels()).getTypedArray();
-            String base64Url = createPngUrl(pixmap.getWidth(), pixmap.getHeight(), typedArray);
-
+            String base64Url = pixmap.canvas.toDataUrl();
             int cut = base64Url.indexOf(',');
             byte[] decoded = Base64Coder.decode(base64Url.substring(cut + 1));
             OutputStream os = fileHandle.write(false);
@@ -24,13 +22,4 @@ public class PixmapIO {
         }
     }
 
-    static private native String createPngUrl(int width, int height, ArrayBufferView rgba) /*-{
-        var canvas = $doc.createElement("canvas");
-        canvas.width = width;
-        canvas.height = height;
-        var ctx = canvas.getContext("2d");
-        var imageData = ctx.getImageData(0, 0, width, height);
-        imageData.data.set(rgba);
-        return canvas.toDataURL();
-    }-*/;
 }
